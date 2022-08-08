62b1efe625c23203201595ede8eb2dd8b4ce4812
Roman Krylov <0087897@gmail.com>
2022-08-08 20:39:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.net.StandardSocketOptions;
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int coffeeAmount = 2330;
+        int milkAmount = 3210;
+
+        int cappuccinoMilkRequired = 60;
+        int cappuccinoCoffeeRequired = 15;
+
+        if (coffeeAmount >= cappuccinoCoffeeRequired) {
+            System.out.println("Готовим капучино");
+        } else {
+            System.out.println("Кофе недостаточно :(");
+        }
+    }
+}
