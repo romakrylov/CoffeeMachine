d96976db243b95860f9d7ad9c82beb241227572c
Roman Krylov <0087897@gmail.com>
2022-08-08 21:44:09 +0300

    && ! ||

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,25 @@ public class Main {
     public static void main(String[] args) {
         int coffeeAmount = 2330;
         int milkAmount = 3210;
+        int skimmedMilkAmount = 1290;
+
+        boolean isBlocked = true;
 
         int cappuccinoMilkRequired = 60;
         int cappuccinoCoffeeRequired = 15;
 
-        if (coffeeAmount >= cappuccinoCoffeeRequired &&
-            milkAmount >= cappuccinoMilkRequired) {
-            System.out.println("Готовим капучино");
+        if (!isBlocked && milkAmount >= cappuccinoMilkRequired &&
+            coffeeAmount >= cappuccinoCoffeeRequired) {
+            System.out.println("Готовим кофе");
         } else {
-            System.out.println("Ингридиентов недостаточно :(");
+            System.out.println("Что-то пошло не так :(");
         }
+
+        //if (coffeeAmount >= cappuccinoMilkRequired ||
+        //   skimmedMilkAmount >= cappuccinoMilkRequired) {
+        //    System.out.println("Молока достаточно");
+        //} else {
+        //    System.out.println("Ингридиентов недостаточно :(");
+        //}
     }
 }
