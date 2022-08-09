f60c33035725a9ccc925e057579a91ffa6ed4cf9
Roman Krylov <0087897@gmail.com>
2022-08-09 12:50:46 +0300

    && ! || ()

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,40 @@ public class Main {
         int coffeeAmount = 2330;
         int milkAmount = 3210;
         int skimmedMilkAmount = 1290;
+        int lowLactoseMilkAmount = 1000;
 
         boolean isBlocked = true;
 
         int cappuccinoMilkRequired = 60;
         int cappuccinoCoffeeRequired = 15;
 
-        if (!isBlocked && milkAmount >= cappuccinoMilkRequired &&
-            coffeeAmount >= cappuccinoCoffeeRequired) {
+        // 2.5 Приоритеты и скобки в условиях
+        if (!isBlocked && coffeeAmount >= cappuccinoCoffeeRequired &&
+            (skimmedMilkAmount >= cappuccinoMilkRequired ||
+            milkAmount >= cappuccinoMilkRequired)) {
             System.out.println("Готовим кофе");
         } else {
-            System.out.println("Что-то пошло не так :(");
+           System.out.println("Что-то пошло не так :(");
         }
 
-        //if (coffeeAmount >= cappuccinoMilkRequired ||
+        //перератор или ||
+        //if (skimmedMilkAmount >= cappuccinoMilkRequired ||
+        //    milkAmount >= cappuccinoMilkRequired ||
+        //    lowLactoseMilkAmount >= cappuccinoMilkRequired) {
+        //    System.out.println("Молока достаточно");
+        //} else {
+        //    System.out.println("Молока недостаточно :(");
+        //}
+        // оператор И && и НЕ !
+        //if (!isBlocked && milkAmount >= cappuccinoMilkRequired &&
+        //    coffeeAmount >= cappuccinoCoffeeRequired) {
+        //    System.out.println("Готовим кофе");
+        //} else {
+        //   System.out.println("Что-то пошло не так :(");
+        //}
+
+        // опрератор или ||
+        // if (coffeeAmount >= cappuccinoMilkRequired ||
         //   skimmedMilkAmount >= cappuccinoMilkRequired) {
         //    System.out.println("Молока достаточно");
         //} else {
